aee913e6e8bb397775fc04ed1045f49f990b3d2e
Thiago Thomaz <thiagothomaz4@gmail.com>
2022-10-04 17:43:14 -0300

    fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,39 +4,36 @@ import { Link, Outlet } from "react-router-dom";
 export function Layout() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" fixed="top">
         <Container>
-          <Row>
-            <Navbar.Brand>
-              <img src="img/logo.png" alt="Logo" style={{ width: "70px" }} />
-            </Navbar.Brand>
-          </Row>
-          <Row>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Link to="/" className="nav-link">
-                  Quem Somos
-                </Link>
-                <Link to="/comunidade" className="nav-link">
-                  Comunidade
-                </Link>
-                <Link to="/oficinas" className="nav-link">
-                  Oficinas
-                </Link>
-                <Link to="/empreendedorismo" className="nav-link">
-                  Empreendedorismo
-                </Link>
-                <Link to="/doacoes" className="nav-link">
-                  Doações
-                </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Row>
+          <Navbar.Brand>
+            <img src="img/logo.png" alt="Logo" style={{ width: "70px" }} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Link to="/" className="nav-link">
+                Quem Somos
+              </Link>
+              <Link to="/comunidade" className="nav-link">
+                Comunidade
+              </Link>
+              <Link to="/oficinas" className="nav-link">
+                Oficinas
+              </Link>
+              <Link to="/empreendedorismo" className="nav-link">
+                Empreendedorismo
+              </Link>
+              <Link to="/doacoes" className="nav-link">
+                Doações
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      <Outlet />
+      <div style={{ paddingTop: "100px" }}>
+        <Outlet />
+      </div>
     </div>
   );
 }
